30dc07c7cfc78ba7133ce0b6f99c50c2ac47709c
Sonal Nandurkar <58734700+sonalnandurkar@users.noreply.github.com>
2023-06-16 00:58:53 +0530

    Time: 66 ms (41.79%), Space: 44.4 MB (30.81%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -6,7 +6,7 @@ var reverseWords = function(s) {
      let str = s.split(" ");
        s = "";
        for(let i=str.length-1;i>=0;i--){
-           if(str[i] != "" &&  str[i] != " " ){
+           if(str[i] != ""){
                s += str[i] + " ";
            }
        }

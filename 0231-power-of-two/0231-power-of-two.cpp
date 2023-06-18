170aa0af2a79dd23af488fb6f8f0a9f5a1b020c1
Sonal Nandurkar <58734700+sonalnandurkar@users.noreply.github.com>
2023-06-18 18:40:04 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.48%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,11 +2,20 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
        // return n>0 && !(n & (n-1));
-        for(int i=0;i<=30;i++){
-            if(pow(2,i) == n){
-                return true;
-            }
+        
+        if(n> 0 && (n & (n-1))==0){
+             return true ; 
+        } else {
+            return false;
         }
-        return false;
+                               
+        
+        // for(int i=0;i<=30;i++){
+        //     if(pow(2,i) == n){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        
     }
 };

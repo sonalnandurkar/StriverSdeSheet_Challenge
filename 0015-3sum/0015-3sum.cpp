3a9cacde24465646f4fa9ea46a24334939c78c50
Sonal Nandurkar <58734700+sonalnandurkar@users.noreply.github.com>
2023-06-18 13:11:41 +0530

    Time: 188 ms (64.00%), Space: 26.1 MB (46.98%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,81 +1,59 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& arr) {
-     sort(arr.begin(),arr.end());
-     vector<vector<int>> ans;
-     for(int i=0;i<arr.size();i++){
-        if(i>0 && arr[i] == arr[i-1]){
-            continue;
-        }
-        int current = arr[i],j=i+1,k=arr.size()-1;
-        while(j<k){
-            if(current + arr[j] + arr[k] == 0){
-                // vector<int> temp = {current,arr[j],arr[k]};
-                ans.push_back({current,arr[j],arr[k]});
-                j++;
-                k--;
-                while(j<k && arr[j] == arr[j-1] ){
-                    
-                    j++;
-                    
-                }
-                while(j<k && arr[k] == arr[k+1]){
-                    k--;
-                    
-                }
-                
-                
-            } else if(current + arr[j] + arr[k] < 0){
-                j++;
-            } else {
-                k--;
-                
+        sort(arr.begin(),arr.end());
+        vector<vector<int>> res;
+        
+        for(int i=0;i<arr.size();i++){
+            if(i>0 && arr[i] == arr[i-1]) {
+                continue;
             }
+            int j=i+1;
+            int k=arr.size()-1;
+            while(j<k){
+                int sum = arr[i]+arr[j]+arr[k];
+                if(sum < 0){
+                    j++;
+                } else if(sum > 0){
+                    k--;
+                } else {
+                    vector<int> temp = {arr[i],arr[j],arr[k]};
+                    res.push_back(temp);
+                    j++;
+                    k--;
+                   while(j<k && arr[j] == arr[j-1]){
+                       j++;
+                   }
+                    while(j < k && arr[k] == arr[k+1]){
+                        k--;
+                    }
+                    
+                }
+            }
+          
+            
         }
-         
-         
-     }
-    return ans;
-        
-//         int n = nums.size();
-//         set<vector<int>> res;
-        
-//         for(int i=0;i<n;i++){
-//              set<int> s1;
-//             for(int j=i+1;j<n;j++){
-//                  int third = -(nums[i]+nums[j]);
-//                 if(s1.find(third) != s1.end()){
-//                  vector<int> temp = {nums[i],nums[j],third};
-//                  sort(temp.begin(),temp.end());
-//                  res.insert(temp);
-//                 }
-//                  s1.insert(nums[j]);
-//              }
-//             }
-//         vector<vector<int>> ans(res.begin(),res.end());
-//         return ans;
+          return res;
         
         
-        
-//         vector<vector<int>> temp;
-//         set<vector<int>> res;
-       
-        
-//         for(int i=0;i<nums.size();i++){
-//            for(int j=i+1;j<nums.size();j++){
-//                for(int k=j+1;k<nums.size();k++){
-//                    if(nums[i]+nums[j]+nums[k] == 0){
-//                      vector<int> op = {nums[i],nums[j],nums[k]};
-//                      sort(op.begin(),op.end());
-//                      res.insert(op);
-                     
-//                    }
-//                }
-//            }
+//       set<vector<int>> ans;
+//       vector<vector<int>> res;
+//       for(int i=0;i<arr.size();i++){
+//            set<int> st;
+//           for(int j=i+1;j<arr.size();j++){
+//               int third = -(arr[i]+arr[j]);
+//               if(st.find(third) != st.end()){
+//                   vector<int> temp = {arr[i],arr[j],third};
+//                   sort(temp.begin(),temp.end());
+//                   ans.insert(temp);
+//               }
+//               st.insert(arr[j]);
+              
+//           }
+//       }
+//         for(auto it: ans){
+//             res.push_back(it);
 //         }
-//      for(auto it:res){
-//          temp.push_back(it);
-//      }
-//         return temp;
+//         return res;
     }
 };

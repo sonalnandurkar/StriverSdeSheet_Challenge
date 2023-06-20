ce4914b6d07a2ed88d757c14f848807fa306390c
Sonal Nandurkar <58734700+sonalnandurkar@users.noreply.github.com>
2023-06-21 00:56:18 +0530

    Time: 11 ms (73.68%), Space: 8.1 MB (71.91%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -14,7 +14,7 @@ public:
         }
         ListNode* slow=head;
         ListNode* fast=head;
-        while(fast != NULL && fast->next != NULL){
+        while(fast->next != NULL && fast->next->next != NULL){
             slow = slow->next;
             fast=fast->next->next;
             if(fast == slow) return true;
